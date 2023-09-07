146938a7a8608292101c6bb5cbf3bc35309002ed
Vladimir Klepov <vklepov@ozon.ru>
2023-09-07 18:36:19 +0300

    add loader

--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -1,8 +1,11 @@
 <script>
   import { loadGameData } from '../api';
   import Level from './Level.svelte';
+  import Loader from './Loader.svelte';
 </script>
 
-{#await loadGameData() then data}
+{#await loadGameData()}
+  <Loader />
+{:then data}
   <Level options={{...data, stepCount: 6, stepSize: 4}} />
 {/await}
