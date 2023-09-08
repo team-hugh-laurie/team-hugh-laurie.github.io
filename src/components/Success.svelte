dcea9a1a6839f19360ed0ebd0df1c722a069dc53
Vladimir Klepov <vklepov@ozon.ru>
2023-09-08 11:06:21 +0300

    more fun bg

--- src/components/Success.svelte
+++ src/components/Success.svelte
@@ -2,9 +2,14 @@
   export let restart: () => void;
 </script>
 
+<svg class="bg-rays" width="375" height="416" viewBox="0 0 375 416" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M375 0H0V89.4467L188 416L375 89.4467V0Z" fill="#5A33C6"/>
+  <path d="M375 0H0L188 326.5L375 0Z" fill="#5331B3"/>
+</svg>
+
 <h1>Well done!</h1>
 
-<svg xmlns="http://www.w3.org/2000/svg" width="375" height="416">
+<svg class="duck" xmlns="http://www.w3.org/2000/svg" width="375" height="416">
   <use href="#winnerduck" />
 </svg>
 
@@ -31,8 +36,16 @@
     letter-spacing: 0.2px;
   }
 
-  svg {
+  .duck {
     transform-origin: center center;
     animation: 800ms ease jump;
+  }
+
+  .bg-rays {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: -2;
   }
 </style>
